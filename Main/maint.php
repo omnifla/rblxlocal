76bdf945f8373cb0db95a20dee5ea3eede921d48
Carbonapi <Carbonapi1@gmail.com>
2025-07-22 07:32:03 -0700

    added maint.php will me updated soon

---
+++ Main/maint.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head> 
+<link rel="stylesheet" href="/CSS/maint.css">
+<title>Site Offline</title>
+</head>
+<body>
+<div class="header"> 
+</div>   
+<div class="stuff">
+<h4>The site is currently for maintenance and upgrades, Please check back Soon!</h4>
+<input type="text"> <button>RBLX.LOCAL</button>
+</body>
+</html>
