137c0eec6d5e4d07d1a8a0e3f3a7cc2e0e31dab8
TheGuyWhoIsIdiot <157973030+TheGuyWhoIsIdiot@users.noreply.github.com>
2025-07-22 19:51:36 -0300

    Changes on the Register function to comply with PHP 8.4 deprecation
    i hate some changes taken in PHP 8.4, which entirely deprecates the ?type $var = null.

--- config/Depedencies/Authentication.php
+++ config/Depedencies/Authentication.php
@@ -71,7 +71,7 @@ class Authentication {
         }
         return true;
     }
-    public static function Register(string $username, string $password, int $gender, ?string $email = null, ?string $birthdate = null) {
+    public static function Register(string $username, string $password, ?int $gender = 1, ?string $email = "", ?string $birthdate = "1970-01-01") {
         global $conn;
         $filter = new BasicTextFilter();
         $jwt_secret = 'EIJ3ITGJANGHIANSGOIJ';
