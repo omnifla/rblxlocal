bbd99a82c8b63865849d30b7d3869b38d2d07146
TheGuyWhoIsIdiot <157973030+TheGuyWhoIsIdiot@users.noreply.github.com>
2025-07-22 14:24:10 -0300

    test

--- Main/CSS/Base/CSS/FetchCSS.php
+++ Main/CSS/Base/CSS/FetchCSS.php
@@ -55,7 +55,7 @@ if (isset($css_bundle_hashes[$path])) {
         }
         if (file_exists($realPath) && is_readable($realPath)) {
             $content = file_get_contents($realPath);
-            $cssContents .= "/* ".$cssPath . " */\n". $content . "\n";
+            $cssContents .= "/* ".$cssPath . " */\n". trim($content, "\xEF\xBB\xBF") . "\n";
             $commentPath = extractCssPath($content);
             if ($commentPath) {
                 $cssFiles[] = trim($commentPath);
