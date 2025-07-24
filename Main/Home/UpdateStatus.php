ec037abe49752fc2bb1962a9d1cd13cba8f6556c
TrapzOfficial <clementdud57@gmail.com>
2025-07-24 18:29:49 +0200

    Update UpdateStatus.php
    limit , char

--- Main/Home/UpdateStatus.php
+++ Main/Home/UpdateStatus.php
@@ -1,5 +1,4 @@
 <?php
-// written by meditext
 include_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
 use Roblox\Authentication as Auth;
 use Roblox\TextFilter\BasicTextFilter;
@@ -13,14 +12,15 @@ $user = Auth::GetAuthenticatedUserInfo();
 if ($status === null || trim($status) === '') {
     exit('{"success": false, "message": "Status cannot be empty."}');
 }
+if (mb_strlen($status) > 280) {
+    exit('{"success": false, "message": "Status must be 280 characters or less."}');
+}
 $status = $filter->filter($status)->getFilteredText();
 $time_posted = time();
-
 $insert_stmt = $conn->prepare("INSERT INTO feeds (author_id, content, posted_at) VALUES (:user_id, :status, :created_at)");
 $insert_stmt->bindParam(':user_id', $user['id'], PDO::PARAM_INT);
 $insert_stmt->bindParam(':status', $status, PDO::PARAM_STR);
 $insert_stmt->bindParam(':created_at', $time_posted, PDO::PARAM_STR);
-
 if ($insert_stmt->execute()) { 
     exit('{"success": true, "message": "Status updated successfully."}');
 } else {
