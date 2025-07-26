d811d25875e457672fc13947ccad5da99603820a
Aidan2662 <111140166+Aidan2662@users.noreply.github.com>
2025-07-25 19:24:08 -0500

    Create Another Page.

---
+++ Main/Login/NewAge.aspx.php
@@ -0,0 +1,19 @@
+<?php
+// written by denied_id
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
+
+header("Location: /NewLogin");
+
+exit;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head></head>
+
+<body>
+    <p>This page was intentionally left empty.</p>
+    <h2>You should be redirected shortly!</h2>
+</body>
+
+</html>
