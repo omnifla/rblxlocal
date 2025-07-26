925a11d5df9977200e50e4023d09392001cc29c4
RandomYaps <156994100+randomyaps@users.noreply.github.com>
2025-07-26 02:25:07 +0200

    Update index.php

--- Main/index.php
+++ Main/index.php
@@ -1,28 +1,14 @@
 <?php
-// written by meditext
+// random 2025
+// the INDEX... 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
+use Roblox\Settings;
 
-if (Roblox\Authentication::GetAuthenticatedUser()) {
-    header("Location: /Home");
-    exit;
+$settings = new Settings();
+
+if ($settings->settings['LandingRedirect']) {
+    header('Location: /Landing/Animated');
+} else {
+    header('Location: /Default.aspx');
 }
-header("Location: /Landing/Animated");
-// header("Location: /Default.aspx.php");
-// Don't uncomment the above line until proper signup/login is implemented.
 exit;
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head></head>
-
-<body>
-    <p>This page was intentionally left empty.</p>
-    <h2>Your Probably looking for Landing/animated.php, Wait how did you get here?</h2>
-</body>
-
-</html>
-
-<?php
-print_r(Roblox\Authentication::GetAuthenticatedUser());
-?>
