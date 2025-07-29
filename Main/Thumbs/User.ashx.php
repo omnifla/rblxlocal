ff1aa268035217ed661493c322b63eaa97e5748a
Chloe <kqsane0401@gmail.com>
2025-07-29 10:03:26 +0930

    Create User.ashx.php

---
+++ Main/Thumbs/User.ashx.php
@@ -0,0 +1,23 @@
+<?php
+$id = $_GET['ID'] ?? '';
+$width = $_GET['Width'] ?? '';
+$height = $_GET['Height'] ?? '';
+
+$placeholder = '/Images/Placeholder1024x1024.png';
+$imagePath = __DIR__ . "/RenderedUsers/$id.png";
+
+if (!preg_match('/^\d+$/', $id) || !preg_match('/^\d+$/', $width) || !preg_match('/^\d+$/', $height)) {
+    $src = $placeholder;
+    $width = 352;
+    $height = 352;
+} elseif (file_exists($imagePath)) {
+    $src = "/Thumbs/RenderedUsers/$id.png";
+} else {
+    $src = $placeholder;
+    $width = 352;
+    $height = 352;
+}
+
+header('Content-Type: image/png');
+readfile($_SERVER['DOCUMENT_ROOT'] . $src);
+?>
