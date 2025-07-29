3a32f9080a52dc34bef947fdd98f9b0369665219
Chloe <kqsane0401@gmail.com>
2025-07-29 10:00:47 +0930

    Update Asset.ashx.php

---
+++ Main/Thumbs/Asset.ashx.php
@@ -0,0 +1,24 @@
+<?php
+// writen by chloe
+$id = $_GET['ID'] ?? '';
+$width = $_GET['Width'] ?? '';
+$height = $_GET['Height'] ?? '';
+
+$placeholder = '/Images/Placeholder1024x1024.png';
+$imagePath = __DIR__ . "/RenderedAssets/$id.png";
+
+if (!preg_match('/^\d+$/', $id) || !preg_match('/^\d+$/', $width) || !preg_match('/^\d+$/', $height)) {
+    $src = $placeholder;
+    $width = 352;
+    $height = 352;
+} elseif (file_exists($imagePath)) {
+    $src = "/Thumbs/RenderedAssets/$id.png";
+} else {
+    $src = $placeholder;
+    $width = 352;
+    $height = 352;
+}
+
+header('Content-Type: text/html');
+echo "<img src=\"$src\" width=\"$width\" height=\"$height\" />";
+?>
