f4e10f4829fa711f0a26bccaa13cba77b26f3ed3
Skyler <dsatakedown85@gmail.com>
2025-07-29 13:31:01 +0100

    Update FeedDAL.php

--- config/Depedencies/Roblox/DataAccess/FeedDAL.php
+++ config/Depedencies/Roblox/DataAccess/FeedDAL.php
@@ -3,6 +3,8 @@
 // This is a ported version of Roblox.DataAccess.FeedDAL intended to be used for a new Feed System in the near future
 include_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
 
+namespace Roblox.DataAccess
+
 class FeedDAL
 {
     public int $post_id = 0;
