887138326e0a5a296bd242c8836979db1875ae81
Skyler <dsatakedown85@gmail.com>
2025-07-30 15:02:53 +0100

    Update UserFeedDAL.php

--- config/Depedencies/Roblox/DataAccess/UserFeedDAL.php
+++ config/Depedencies/Roblox/DataAccess/UserFeedDAL.php
@@ -1,7 +1,7 @@
 <?php
 // written and ported by SkylerClock
-// This is a ported version of Roblox.DataAccess.FeedDAL intended to be used for a new Feed System in the near future
-namespace Roblox.DataAccess;
+// This is a ported version of Roblox\DataAccess\FeedDAL intended to be used for a new Feed System in the near future
+namespace Roblox\DataAccess;
 
 class UserFeedDAL
 {
