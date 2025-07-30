5e62c03a4e8052f10bbd9ef7c685d2db3f7139e8
Skyler <dsatakedown85@gmail.com>
2025-07-30 22:39:23 +0100

    Update GetAlert.php

--- Main/v1/GetAlert.php
+++ Main/v1/GetAlert.php
@@ -4,7 +4,7 @@ use Roblox\Alert;
 $alerts = \Roblox\Alert::getMostRecentAlertsPaged(0, 3);
 if (empty($alerts)) return;
 foreach ($alerts as $alert) {
-    $text = htmlspecialchars($alert->Text);
+    $text = htmlspecialchars($alert->getText());
     echo <<<HTML
     <div class="SystemAlert" style="background-color: orange;"><div class="SystemAlertText">{$text}</div></div>
     HTML;
