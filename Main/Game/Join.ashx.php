f3e5c2799cbfe1f8883b3b1c0f431ae228ea2782
Skyler <dsatakedown85@gmail.com>
2025-07-30 11:13:13 +0100

    Update Join.ashx.php

--- Main/Game/Join.ashx.php
+++ Main/Game/Join.ashx.php
@@ -1,2 +1,8 @@
 <?php
-// stub for now
+include_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
+use Roblox\Game\ClientScriptCreator;
+use Roblox\Game\ClientHelper;
+ClientHelper::init();
+$rawScript = ClientScriptCreator::getScript('join', ClientScriptCreator::$DEFAULT_REPLACELIST);
+$signedScript = ClientHelper::signTextBlob($rawScript);
+exit($signedScript);
