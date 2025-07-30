8beecb16d7439d7e3be597370fdb4cd9cda85621
Skyler <dsatakedown85@gmail.com>
2025-07-30 22:33:08 +0100

    Create GetAlert.php

---
+++ Main/v1/GetAlert.php
@@ -0,0 +1,12 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
+use Roblox\Alert;
+$alerts = \Roblox\Alert::getMostRecentAlertsPaged(0, 3);
+if (empty($alerts)) return;
+foreach ($alerts as $alert) {
+    $text = htmlspecialchars($alert->Text);
+    echo <<<HTML
+    <div class="SystemAlert" style="background-color: orange;"><div class="SystemAlertText">{$text}</div></div>
+    HTML;
+}
+?>
