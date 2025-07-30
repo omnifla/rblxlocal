15206bd4398de39a1efdae220620f0ac1519377c
Skyler <dsatakedown85@gmail.com>
2025-07-30 14:46:42 +0100

    Update GetUserFeed.php

--- Main/Feeds/GetUserFeed.php
+++ Main/Feeds/GetUserFeed.php
@@ -1,28 +1,47 @@
 <?php
-// written by meditext
+// rewritten by SkylerClock to use the new feed system, originally written by meditext
 include_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
+use Roblox\UserFeed;
 use Roblox\Authentication as Auth;
-$feed_limit = 20;
-$feeds = $conn->query("SELECT * FROM feeds ORDER BY post_id DESC LIMIT $feed_limit")->fetchAll(PDO::FETCH_ASSOC);
-// fetch the feeds
-// if there isnt any feeds, output a message
+$userId = Auth::GetUserID();
+$startIndex = 0;
+$maxRows = 20;
+$feeds = UserFeed::getByUserIdPaged($userId, $startIndex, $maxRows);
 if (empty($feeds)) {
     echo '<div class="no-feeds">No feeds available.</div>';
     exit;
 }
-foreach ($feeds as $feed) {
-    $feed_content = htmlspecialchars($feed['content']);
-    $feed_date = date('m/d/Y', $feed['posted_at']) . " at " . date('h:i A', $feed['posted_at']);
-    $user = Auth::GetUserInfo($feed['author_id']);
-    if (!$user) {
-        continue; // skip if user not found
-    }
+foreach ($feeds as $userFeed) {
+    $feedDAL = \Roblox\DataAccess\FeedDAL::get($userFeed->getFeedId());
+    if (!$feedDAL) continue;
+    $user = Auth::GetUserInfo($feedDAL->author_id);
+    if (!$user) continue;
     $username = htmlspecialchars($user['username']);
-$html = <<<HTML
-    	        <div class="divider-top feed-container"><div class="feed-image-container notranslate">
-                <a href="http://{$site_properties['hostname']}/user.aspx?id={$user['id']}"><span class="feed-user-avatar"><img alt="{$username}" class="feed-user-avatar-image" src="/Images/Placeholder1024x1024.png" width=50 height=50></span>
-                    </a></div><div class="feed-text-container text"><span class="notranslate"><a href="http://{$site_properties['hostname']}/User.aspx?ID={$user['id']}">{$username}</a><br><div class="Feedtext">"{$feed_content}"</div></span><span style="display: block; padding-top: 5px; color: #AAA; font-size: 11px;">{$feed_date}</span> </div><div class="feed-report-abuse"><a href="http://{$site_properties['hostname']}/AbuseReport/Feed.aspx?ID=64045715&RedirectUrl=/home"><img src="//images.rbxcdn.com/1ea8de3b0f71a67b032b67ddc1770c78.png" alt="Report abuse" id="reportAbuseButton"> </a></div><div class="clear"></div></div>
-    HTML;
-    echo $html;
+    $content = htmlspecialchars($feedDAL->content);
+    $timestamp = date('m/d/Y', $feedDAL->posted_at) . ' at ' . date('h:i A', $feedDAL->posted_at);
+    $html = <<<HTML
+<div class="divider-top feed-container">
+    <div class="feed-image-container notranslate">
+        <a href="/user.aspx?id={$user['id']}">
+            <span class="feed-user-avatar">
+                <img alt="{$username}" class="feed-user-avatar-image" src="/Images/Placeholder1024x1024.png" width=50 height=50>
+            </span>
+        </a>
+    </div>
+    <div class="feed-text-container text">
+        <span class="notranslate">
+            <a href="/User.aspx?ID={$user['id']}">{$username}</a><br>
+            <div class="Feedtext">"{$content}"</div>
+        </span>
+        <span style="display: block; padding-top: 5px; color: #AAA; font-size: 11px;">{$timestamp}</span>
+    </div>
+    <div class="feed-report-abuse">
+        <a href="/AbuseReport/Feed.aspx?ID={$feedDAL->post_id}&RedirectUrl=/home">
+            <img src="//images.rbxcdn.com/1ea8de3b0f71a67b032b67ddc1770c78.png" alt="Report abuse">
+        </a>
+    </div>
+    <div class="clear"></div>
+</div>
+HTML;
+echo $html;
 }
-?>
