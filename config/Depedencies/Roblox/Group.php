1021698bebfc4216b818306f9ae0b11605fff747
Skyler <dsatakedown85@gmail.com>
2025-08-01 12:15:02 +0100

    update group port as i got drunk yesterday

--- config/Depedencies/Roblox/Group.php
+++ config/Depedencies/Roblox/Group.php
@@ -1,201 +1,99 @@
 <?php
-// written and ported by SkylerClock
-namespace Roblox;
+namespace Roblox\Platform\Groups;
 
-use Roblox\DataAccess\GroupDAL;
-use Roblox\Helpers\EntityHelper;
-use Roblox\Enums\CreatorType;
-use DateTime;
-use Exception;
+use Roblox\Group as GroupEntity;
+use Roblox\Platform\Membership\IUser;
+use Roblox\Platform\Groups\Events\GroupEventType;
+use Roblox\Platform\Groups\GroupManagement;
+use Roblox\Enums\GroupActionType;
 
-class Group
+class Group implements IGroup
 {
-    private GroupDAL $entityDAL;
+    private ?bool $isLocked = null;
+    
+    public int $id;
+    public int $agentId;
+    public ?int $ownerUserId;
+    public ?int $previousOwnerUserId;
+    public string $name;
+    public int $emblemId;
+    public bool $publicEntryAllowed;
+    public bool $bcOnly;
+    public bool $hasClan;
+    public \DateTime $created;
+    public \DateTime $updated;
+    public string $description;
 
-    public function __construct(?GroupDAL $dal = null)
-    {
-        $this->entityDAL = $dal ?? new GroupDAL();
-    }
+    private GroupDomainFactories $domainFactories;
 
-    public function getID(): int
-    {
-        return $this->entityDAL->ID;
-    }
-
-    public function getName(): string
-    {
-        return $this->entityDAL->Name;
-    }
-
-    public function setName(string $name): void
-    {
-        $this->entityDAL->Name = $name;
-    }
-
-    public function getCreatorType(): int
-    {
-        return CreatorType::Group;
-    }
-
-    public function getAgentID(): ?int
-    {
-        return $this->entityDAL->AgentID;
-    }
-
-    public function setAgentID(?int $id): void
-    {
-        $this->entityDAL->AgentID = $id;
-    }
-
-    public function getOwnerUserID(): ?int
-    {
-        return $this->entityDAL->OwnerUserID;
-    }
-
-    public function setOwnerUserID(?int $id): void
-    {
-        $this->entityDAL->OwnerUserID = $id;
-    }
-
-    public function getPreviousOwnerUserID(): int
-    {
-        return $this->entityDAL->PreviousOwnerUserID;
-    }
-
-    public function setPreviousOwnerUserID(int $id): void
-    {
-        $this->entityDAL->PreviousOwnerUserID = $id;
-    }
-
-    public function getDescription(): string
-    {
-        return $this->entityDAL->Description;
-    }
-
-    public function setDescription(string $desc): void
-    {
-        $this->entityDAL->Description = $desc;
-    }
-
-    public function getEmblemID(): int
-    {
-        return $this->entityDAL->EmblemID;
-    }
-
-    public function setEmblemID(int $id): void
-    {
-        $this->entityDAL->EmblemID = $id;
-    }
-
-    public function isPublicEntryAllowed(): bool
-    {
-        return $this->entityDAL->PublicEntryAllowed;
-    }
-
-    public function setPublicEntryAllowed(bool $val): void
-    {
-        $this->entityDAL->PublicEntryAllowed = $val;
-    }
-
-    public function isBCOnlyJoin(): bool
-    {
-        return $this->entityDAL->BCOnlyJoin;
-    }
-
-    public function setBCOnlyJoin(bool $val): void
-    {
-        $this->entityDAL->BCOnlyJoin = $val;
+    public function __construct(
+        IGroupEntity $groupEntity,
+        bool $hasClan,
+        GroupDomainFactories $domainFactories
+    ) {
+        $this->id = $groupEntity->Id;
+        $this->agentId = $groupEntity->AgentId;
+        $this->ownerUserId = $groupEntity->OwnerUserId;
+        $this->previousOwnerUserId = $groupEntity->PreviousOwnerUserId;
+        $this->name = $groupEntity->Name;
+        $this->emblemId = $groupEntity->EmblemId;
+        $this->created = $groupEntity->Created;
+        $this->updated = $groupEntity->Updated;
+        $this->description = $groupEntity->Description;
+        $this->publicEntryAllowed = $groupEntity->PublicEntryAllowed;
+        $this->bcOnly = $groupEntity->BCOnly;
+        $this->hasClan = $hasClan;
+        $this->domainFactories = $domainFactories;
     }
 
     public function isLocked(): bool
     {
-        return $this->entityDAL->IsLocked;
-    }
-
-    public function setLocked(bool $val): void
-    {
-        $this->entityDAL->IsLocked = $val;
-    }
-
-    public function getCreated(): DateTime
-    {
-        return $this->entityDAL->Created;
-    }
-
-    public function getUpdated(): DateTime
-    {
-        return $this->entityDAL->Updated;
-    }
-
-    public function lock(): void
-    {
-        $this->setLocked(true);
-        $this->save();
-    }
-
-    public function unlock(): void
-    {
-        $this->setLocked(false);
-        $this->save();
-    }
-
-    public function save(): void
-    {
-        EntityHelper::saveEntity($this, function () {
-            $now = new DateTime();
-            $this->entityDAL->Created = $now;
-            $this->entityDAL->Updated = $now;
-            $this->entityDAL->insert();
-        }, function () {
-            $this->entityDAL->Updated = new DateTime();
-            $this->entityDAL->update();
-        });
-    }
-
-    public function delete(): void
-    {
-        EntityHelper::deleteEntity($this, fn () => $this->entityDAL->delete());
-    }
-
-    public static function createNew(int $ownerId, string $name, string $desc, int $emblemId, bool $publicJoin, bool $bcOnly): Group
-    {
-        $group = new Group();
-        $group->setOwnerUserID($ownerId);
-        $group->setPreviousOwnerUserID($ownerId);
-        $group->setName($name);
-        $group->setDescription($desc);
-        $group->setEmblemID($emblemId);
-        $group->setPublicEntryAllowed($publicJoin);
-        $group->setBCOnlyJoin($bcOnly);
-        $group->setLocked(false);
-        $group->save();
-
-        try {
-            $agentId = User::getOrCreateGroupAgentId($group->getID());
-            $group->setAgentID($agentId);
-            $group->save();
-        } catch (Exception $e) {
-            $group->delete();
-            throw $e;
+        if ($this->isLocked === null) {
+            $db = GroupEntity::get($this->id);
+            $this->isLocked = $db ? $db->isLocked() : false;
         }
-
-        return $group;
+        return $this->isLocked;
     }
 
-    public static function get(int $id): ?Group
+    public function unlock(IUser $user): void
     {
-        $dal = GroupDAL::get($id);
-        return $dal ? new Group($dal) : null;
+        $db = GroupEntity::get($this->id);
+        if ($db) {
+            $db->unlock();
+            GroupManagement::logGroupAction(
+                Settings::get('RobloxUserId'),
+                $this->id,
+                GroupActionType::Lock()->getID(),
+                ['InitiatorId' => $user->getId()]
+            );
+            $this->isLocked = $db->isLocked();
+        }
+        $this->domainFactories->getGroupEventPublisher()
+            ->publish($this->id, GroupEventType::Updated(), null);
     }
 
-    public static function getByName(string $name): ?Group
+    public function lock(IUser $user): void
     {
-        $dal = GroupDAL::getByName($name);
-        return $dal ? new Group($dal) : null;
+        $db = GroupEntity::get($this->id);
+        if ($db) {
+            $db->lock();
+            GroupManagement::logGroupAction(
+                Settings::get('RobloxUserId'),
+                $this->id,
+                GroupActionType::Lock()->getID(),
+                ['InitiatorId' => $user->getId()]
+            );
+            $this->isLocked = $db->isLocked();
+        }
+        $this->domainFactories->getGroupEventPublisher()
+            ->publish($this->id, GroupEventType::Updated(), null);
     }
 
-    public function construct(GroupDAL $dal): void
+    public function getGroupRoleSetByUser(IUser $user)
     {
-        $this->entityDAL = $dal;
+        $entity = $this->domainFactories->getGroupRoleSetEntityFactory()
+                       ->getByGroupIdAndUserId($this->id, $user?->getId());
+        return $this->domainFactories->getGroupRoleSetAccessorInternal()
+                    ->getByEntity($entity);
     }
 }
