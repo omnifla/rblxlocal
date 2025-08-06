464ecda925747bfe8ec98accf4f49413382d7e46
TheGuyWhoIsIdiot <157973030+TheGuyWhoIsIdiot@users.noreply.github.com>
2025-08-06 16:47:09 -0300

    hmm, i think i forgot something.

--- Main/My/Account.aspx.php
+++ Main/My/Account.aspx.php
@@ -567,6 +567,38 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       }
   });
 </script>
+<div class="ConfirmationModal modalPopup unifiedModal smallModal" data-modal-handle="confirmation" style="display:none;">
+    <a class="genericmodal-close ImageButton closeBtnCircle_20h"></a>
+    <div class="Title"></div>
+    <div class="GenericModalBody">
+        <div class="TopBody">
+            <div class="ImageContainer roblox-item-image" data-image-size="small" data-no-overlays data-no-click>
+                <img class="GenericModalImage" alt="generic image" />
+            </div>
+            <div class="Message"></div>
+        </div>
+        <div class="ConfirmationModalButtonContainer">
+            <a href id="roblox-confirm-btn"><span></span></a>
+            <a href id="roblox-decline-btn"><span></span></a>
+        </div>
+        <div class="ConfirmationModalFooter">
+        
+        </div>  
+    </div>  
+    <script type="text/javascript">
+        var Roblox = Roblox || {};
+        Roblox.GenericConfirmation = Roblox.GenericConfirmation || {};
+        
+        //<sl:translate>
+        Roblox.GenericConfirmation.Resources = {
+            yes: "Yes",
+            No: "No",
+            Confirm: "Confirm",
+            Cancel: "Cancel"
+        };
+        //</sl:translate>
+    </script>
+</div>
 <div style="clear:both"></div>
   </div></div></div>
 </div>
