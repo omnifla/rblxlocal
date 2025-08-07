e67d1f3138fd8e3919e6949bddeae353ee8ae90a
Skyler <dsatakedown85@gmail.com>
2025-08-07 22:56:40 +0100

    Update get.php

--- Main/feedifications/get.php
+++ Main/feedifications/get.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
 use Roblox\DataAccess\FeedificationDAL;
 $feed = FeedificationDAL::getRecent(1);
