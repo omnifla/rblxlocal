25e353df1519c02784d8757b90475e1d2082c1fa
Skyler <dsatakedown85@gmail.com>
2025-08-07 23:16:51 +0100

    Update SiteHeader.php

--- config/Depedencies/Roblox/Web/SiteHeader.php
+++ config/Depedencies/Roblox/Web/SiteHeader.php
@@ -1207,9 +1207,13 @@ class SiteAlert {
         }
         $text = htmlspecialchars($alert->getText(), ENT_QUOTES, 'UTF-8');
         return <<<HTML
-        <div class="SystemAlert">
-            <div class="SystemAlertText" style="background-color: orange;">{$text}</div>
-        </div>
+        <div id="ctl00_Announcement">
+    	  <div id="ctl00_SystemAlertDiv" class="SystemAlert" style="background-color: orange;">
+    	    <div id="ctl00_SystemAlertTextColor" class="SystemAlertText">
+     	     <div id="ctl00_LabelAnnouncement">{$text}</div>
+     	   </div>
+     	 </div>
+    	</div>
         HTML;
     }
 }
