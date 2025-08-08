5ba436c2c0f5bdd3a2d437109b74d2c010f0dcb9
Skyler <dsatakedown85@gmail.com>
2025-08-08 12:05:09 +0100

    Create FriendingEventType.php

---
+++ config/Depedencies/Roblox/Social/FriendingEventType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Roblox\Social;
+
+final class FriendingEventType
+{
+    public const ACCEPT = 'accept';
+    public const IGNORE = 'ignore';
+    public const REQUEST = 'request';
+    public const UNFRIEND = 'unfriend';
+
+    private function __construct()
+    {
+        //
+    }
+}
