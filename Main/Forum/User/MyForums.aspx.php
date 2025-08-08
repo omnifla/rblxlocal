0f896dd168458ee892725b248ce7900dc648d1eb
Skyler <dsatakedown85@gmail.com>
2025-08-08 14:06:12 +0100

    Update MyForums.aspx.php

--- Main/Forum/User/MyForums.aspx.php
+++ Main/Forum/User/MyForums.aspx.php
@@ -31,23 +31,113 @@ use Roblox\Web\SiteAlert;
                                     </nobr>
                                 </div>
                             </span></td>
-                        <td align="right"><span id="ctl00_cphRoblox_ThreadView1_ctl00_Navigationmenu1">
+                        <td align="right"><span id="ctl00_cphRoblox_ThreadView1">
 
-                                <div id="forum-nav" style="text-align: right">
-                                    <a id="ctl00_cphRoblox_ThreadView1_ctl00_Navigationmenu1_ctl00_HomeMenu" class="menuTextLink first" href="/Forum/Default.aspx">Home</a>
-                                    <a id="ctl00_cphRoblox_ThreadView1_ctl00_Navigationmenu1_ctl00_SearchMenu" class="menuTextLink" href="/Forum/Search/default.aspx">Search</a>
-                                </div>
-                            </span></td>
-                    </tr>
-                    <td class="LeftColumn">&nbsp;</td>
-                    <td id="ctl00_cphRoblox_CenterColumn" class="CenterColumn">
-                        <br>
-                        <table class="table" width="100%" cellpadding="2" cellspacing="1" border="0">
-                            <div style="height:7px;"></div>
-                            <tr class="table-header forum-table-header">
-                                <th class="first" colspan="2" width="55%">&nbsp;Subject</th>
-                                <th style="width:15%;" align="center">Author</th>
-                                <th style="width:5%;" align="center">Replies</th>
-                                <th style="width:5%;" align="center">Views</th>
-                                <th style="width:20%;" align="center">Last Post&nbsp;</th>
-                            </tr>
+<table cellpadding="0" width="100%">
+	<tbody><tr>
+		<td align="left"><span id="ctl00_cphRoblox_ThreadView1_ctl00_Whereami1" name="Whereami1">
+<div>
+    <nobr>
+        <a id="ctl00_cphRoblox_ThreadView1_ctl00_Whereami1_ctl00_LinkHome" class="linkMenuSink notranslate" href="/Forum/Default.aspx">ROBLOX Forum</a>
+    </nobr>
+</div></span></td>
+        <td align="right"><span id="ctl00_cphRoblox_ThreadView1_ctl00_Navigationmenu1">
+
+<div id="forum-nav" style="text-align: right">
+	<a id="ctl00_cphRoblox_ThreadView1_ctl00_Navigationmenu1_ctl00_HomeMenu" class="menuTextLink first" href="/Forum/Default.aspx">Home</a>
+	<a id="ctl00_cphRoblox_ThreadView1_ctl00_Navigationmenu1_ctl00_SearchMenu" class="menuTextLink" href="/Forum/Search/default.aspx">Search</a>
+	
+	
+	
+	
+	
+	
+	
+</div>
+</span></td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;
+		</td>
+	</tr>
+	<tr style="padding-bottom:5px;">
+		<td valign="bottom" align="left">
+		    <a id="ctl00_cphRoblox_ThreadView1_ctl00_NewThreadLinkTop" class="btn-control btn-control-medium verified-email-act" href="/web/20151106063046/http://forum.roblox.com/Forum/AddPost.aspx?ForumID=46">
+				New Thread
+			</a>
+		</td>
+		<td align="right">
+		    <span class="normalTextSmallBold">Search this forum: </span>
+			<input name="ctl00$cphRoblox$ThreadView1$ctl00$Search" type="text" id="ctl00_cphRoblox_ThreadView1_ctl00_Search">
+			<input type="submit" name="ctl00$cphRoblox$ThreadView1$ctl00$SearchButton" value=" Go " id="ctl00_cphRoblox_ThreadView1_ctl00_SearchButton" class="translate btn-control btn-control-medium forum-btn-control-medium">
+        </td>
+	</tr>
+	<tr>
+		<td valign="top" colspan="2">
+		    <div style="height:7px"></div>
+		    <table id="ctl00_cphRoblox_ThreadView1_ctl00_ThreadList" class="tableBorder" cellspacing="1" cellpadding="3" border="0" style="width:100%;">
+	<tbody><tr class="forum-table-header">
+		<th align="left" colspan="3" style="height:25px;">&nbsp;Subject&nbsp;</th><th align="left" style="white-space:nowrap;">&nbsp;Author&nbsp;</th><th align="center">&nbsp;Replies&nbsp;</th><th align="center">&nbsp;Views&nbsp;</th><th align="center" style="white-space:nowrap;">&nbsp;Last Post&nbsp;</th>
+	</tr><tr class="forum-table-footer">
+		<td colspan="7">&nbsp;</td>
+	</tr>
+</tbody></table>
+            <span id="ctl00_cphRoblox_ThreadView1_ctl00_Pager"><table cellspacing="0" cellpadding="0" border="0" style="width:100%;border-collapse:collapse;">
+	<tbody><tr>
+		<td><span class="normalTextSmallBold">Page 1 of 40,948</span></td><td align="right"><span><span class="normalTextSmallBold">Goto to page: </span><a id="ctl00_cphRoblox_ThreadView1_ctl00_Pager_Page0" class="normalTextSmallBold" href="javascript:__doPostBack('ctl00$cphRoblox$ThreadView1$ctl00$Pager$Page0','')">1</a><span class="normalTextSmallBold">, </span><a id="ctl00_cphRoblox_ThreadView1_ctl00_Pager_Page1" class="normalTextSmallBold" href="javascript:__doPostBack('ctl00$cphRoblox$ThreadView1$ctl00$Pager$Page1','')">2</a><span class="normalTextSmallBold">, </span><a id="ctl00_cphRoblox_ThreadView1_ctl00_Pager_Page2" class="normalTextSmallBold" href="javascript:__doPostBack('ctl00$cphRoblox$ThreadView1$ctl00$Pager$Page2','')">3</a><span class="normalTextSmallBold"> ... </span><a id="ctl00_cphRoblox_ThreadView1_ctl00_Pager_Page40946" class="normalTextSmallBold" href="javascript:__doPostBack('ctl00$cphRoblox$ThreadView1$ctl00$Pager$Page40946','')">40,947</a><span class="normalTextSmallBold">, </span><a id="ctl00_cphRoblox_ThreadView1_ctl00_Pager_Page40947" class="normalTextSmallBold" href="javascript:__doPostBack('ctl00$cphRoblox$ThreadView1$ctl00$Pager$Page40947','')">40,948</a><span class="normalTextSmallBold">&nbsp;</span><a id="ctl00_cphRoblox_ThreadView1_ctl00_Pager_Next" class="normalTextSmallBold" href="javascript:__doPostBack('ctl00$cphRoblox$ThreadView1$ctl00$Pager$Next','')">Next</a></span></td>
+	</tr>
+</tbody></table></span>
+            
+            
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			&nbsp;
+		</td>
+	</tr>
+	<tr>
+		<td align="left" valign="top">
+			<span id="ctl00_cphRoblox_ThreadView1_ctl00_Whereami2" name="Whereami2">
+<div>
+    <nobr>
+        <a id="ctl00_cphRoblox_ThreadView1_ctl00_Whereami2_ctl00_LinkHome" class="linkMenuSink notranslate" href="/web/20151106063046/http://forum.roblox.com/Forum/Default.aspx">ROBLOX Forum</a>
+    </nobr>
+    <nobr>
+        <span id="ctl00_cphRoblox_ThreadView1_ctl00_Whereami2_ctl00_ForumGroupSeparator" class="normalTextSmallBold"> » </span>
+        <a id="ctl00_cphRoblox_ThreadView1_ctl00_Whereami2_ctl00_LinkForumGroup" class="linkMenuSink notranslate" href="/web/20151106063046/http://forum.roblox.com/Forum/ShowForumGroup.aspx?ForumGroupID=1">ROBLOX</a>
+    </nobr>
+    <nobr>
+        <span id="ctl00_cphRoblox_ThreadView1_ctl00_Whereami2_ctl00_ForumSeparator" class="normalTextSmallBold"> » </span>
+        <a id="ctl00_cphRoblox_ThreadView1_ctl00_Whereami2_ctl00_LinkForum" class="linkMenuSink notranslate" href="/web/20151106063046/http://forum.roblox.com/Forum/ShowForum.aspx?ForumID=46">All Things ROBLOX</a>
+    </nobr>
+</div></span>
+			
+		</td>
+		<td align="right">
+			<span class="normalTextSmallBold">Display threads for: </span><select name="ctl00$cphRoblox$ThreadView1$ctl00$DisplayByDays" id="ctl00_cphRoblox_ThreadView1_ctl00_DisplayByDays">
+	<option selected="selected" value="0">All Days</option>
+	<option value="1">Today</option>
+	<option value="3">Past 3 Days</option>
+	<option value="7">Past Week</option>
+	<option value="14">Past 2 Weeks</option>
+	<option value="30">Past Month</option>
+	<option value="90">Past 3 Months</option>
+	<option value="180">Past 6 Months</option>
+	<option value="360">Past Year</option>
+
+</select>
+			<br>
+			    <a id="ctl00_cphRoblox_ThreadView1_ctl00_MarkAllRead" class="linkSmallBold" href="javascript:__doPostBack('ctl00$cphRoblox$ThreadView1$ctl00$MarkAllRead','')">Mark all threads as read</a>
+			<br>
+			<span class="normalTextSmallBold">
+				
+			</span>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2">&nbsp;</td>
+	</tr>
+</tbody></table>
+</span>
