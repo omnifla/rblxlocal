c3cf00cecb27faeb0d402b9f2a1fba58dab24519
Skyler <dsatakedown85@gmail.com>
2025-08-09 18:08:42 +0100

    Update terms-of-service.php

--- Main/info/terms-of-service.php
+++ Main/info/terms-of-service.php
@@ -56,79 +56,38 @@ $user = Auth::GetAuthenticatedUserInfo();
     <div ><h2>Terms Of Service</h2></div>
     <div>
         <p>
-            It is very important that you (and your parents, if you're under 18) review the
-            following rules so that you know what you can and cannot do on ROBLOX. By using
+            It is very important that you review the
+            following rules so that you know what you can and cannot do on RBLX.local. By using
             this site, you and your parents agree to abide by the following terms and conditions:
         </p>
         <div style="padding:0 20px;">
            <div class="tos-section">
-                1. All sales of Roblox products and services are final, including virtual items and currency.No refunds will be given, except 
-                in our sole and absolute discretion.  If you believe an unauthorized charge was made under your account you agree to
-                notify us immediately so that we may take appropriate action.
+                1. You must be 13 or above to access RBLX.local
                 <p>
-                    Permission from a primary account holder for billing must be gained prior to making purchases
-                    at Roblox.com. If permission has not been gained, Roblox reserves the right to cancel subscription accounts 
-                    linked to unapproved purchases.
-                </p>
-                <p>
-                    If your Account or subscription, is terminated by either party, no refund will be granted; no online time or other 
-                    credits will be credited to you, and you will have no further access to your account. Refund requests will not be considered 
-                    after 45 days from the date of purchase.
-                </p>
-                <p>
-                    Game Cards cannot be redeemed for cash, returned for cash refund, exchanged, or redeemed for anything other than a 
-                    limited license to use as virtual currency on Roblox.com. 
-                </p>
-                <p>
-                    Roblox reserves the right to change our terms and conditions from time to time.
-                </p>
-                <p>
-                    To the fullest extent permitted by law, you waive all claims against us related to payments unless you 
-                    submit the claim to us within 45 days after the charge.  If you have questions please contact 
-                    us at <span class="SL_swap" id="CsEmailLink"><a href="mailto:info@roblox.com">info@roblox.com</a></span>. 
-                </p>
-                <p>
-                    All refunds will be issued in U.S. Dollars.
+                    This is a common rule on every Roblox Revival and if we catch you being underage we will terminate your account off from the website.
                 </p>
            </div>
-           <div class="tos-section">
-                    2. The accounts, activities, items, games and models are for you to play with while
-                    on the site. Except as permitted by the functionality of this site, you can't sell
-                    them (for money or ROBLOX Points), give them to anyone, trade them for anything
-                    (including ROBLOX Points), or pretend you made them. You can display our pictures
-                    on your own personal web pages for your personal noncommercial use ONLY as long
-                    as you write either "Copyright 2005 ROBLOX Corporation All Rights Reserved. Used
-                    With Permission" or "(c) 2005 ROBLOX, Inc. All Rights Reserved. Used With Permission"
-                    on EVERY page with our pictures and/or text and you link directly to us. By the
-                    way, this means that you can't sell ads, barter for stuff, offer your services,
-                    or otherwise earn money, ROBLOX' Items, or ROBLOX Points either on web pages that
-                    contain our pictures and other Materials, or by using our pictures or other Materials,
-                    such as by offering to make ROBLOX banners or backgrounds for others.
-                    <p>
-                        If there is anything you are unsure of about this, please contact us at <span class="SL_swap" id="CsEmailLink"><a href="mailto:info@roblox.com">info@roblox.com</a></span> and we will do our best to give you a quick answer.
-                    </p>
-            </div>
             <div class="tos-section">
                 <p>
-                    3. Most models, games and content on the message boards come straight from other
-                    ROBLOX users, not from someone at ROBLOX. If you see anything mean or nasty on the
+                    2. Most models, games and content on the message boards come straight from other
+                    RBLX.local users, not from someone at RBLX.local. If you see anything mean or nasty on the
                     site, or anyone sends you anything that makes you uncomfortable or asks for your
                     password or personal information (such as your name, address or phone number), please
-                    let us know immediately at <span class="SL_swap" id="CsEmailLink"><a href="mailto:info@roblox.com">info@roblox.com</a></span>
+                    let us know immediately at <span class="SL_swap" id="CsEmailLink"><a href="mailto:info@watr13.icu">info@watr13.icu</a></span>
                     so that we can handle it right away! However, reporting false abuse or inappropriate
                     feedback will not be tolerated and will likely result in a frozen account.
                 </p>
             </div>
             <div class="tos-section">
-                4. All user content and communications on the ROBLOX site&nbsp;may be&nbsp;filtered
-                and monitored. ROBLOX does not pre-screen all submitted content, but ROBLOX and its 
+                3. All user content and communications on the RBLX.local site&nbsp;may be&nbsp;filtered
+                and monitored. RBLX.local does not pre-screen all submitted content, but RBLX.local and its 
                 designees shall have the right in their sole discretion to reject or remove any content 
                 that is available via the site.  So that everyone has a good time, you understand and agree that you
                 will not post or send through the site any words, images or links containing or
                 relating to:
                 <p>
                     <ul>
-                        <li>profanity, obscenities ("swear words"), sexual content (express or implied, including
+                        <li>profanity, slurs, sexual content (express or implied, including
                             inappropriate acts with or by your pets for "real" or in role play)</li>
                         <li>attacks, comments, or opinions about other people or things that slander, defame,
                             threaten, insult or harass another person</li>
@@ -143,78 +102,48 @@ $user = Auth::GetAuthenticatedUserInfo();
                             or names that would be considered inappropriate under our Terms and Conditions</li>
                         <li>"cheats" or "hacks", or information or links to sites claiming to have these</li>
                         <li>requests for user passwords</li>
-                        <li>requests for money by using your models, ROBLOX Points or any other ROBLOX property
+                        <li>requests for money by using your models, RBLX.local Points or any other RBLX.local property
                             on third party sites or your personal websites (including Ebay)</li>
                         <li>scams of any kind (including requests to users to change their email address)</li>
                         <li>"spamming" (repeatedly posting the same message) or "party boards"</li>
-                        <li>anything that suggests it's from a member of the ROBLOX staff</li>
-                        <li>other information that ROBLOX deems, in its sole discretion, to be inappropriate
+                        <li>anything that suggests it's from a member of the RBLX.local staff</li>
+                        <li>other information that RBLX.local deems, in its sole discretion, to be inappropriate
                             for this site </li>
                     </ul>
                     <p>
-                        <b>BEWARE:</b> If you do <b>any of the above</b> we may <b>freeze your account permanently without prior notice.</b>
+                        <b>BEWARE:</b> If you do <b>any of the above</b> we may <b>terminate your account without prior notice.</b>
                     </p>
                     <p>&nbsp;</p>
                 </p>
-                <b>Intellectual Property Policy</b><br />
-                ROBLOX respects the intellectual property rights of others and we ask our users to do the same. 
-                ROBLOX complies with all applicable provisions of the Digital Millennium Copyright Act.  
-                If you believe your copyrighted material and/or any of your other intellectual property has 
-                been copied on our Site, please notify ROBLOX’s copyright agent and include the 
-                following information in writing: 
-                <p>
-                    <ol style="list-style-type: lower-alpha">
-                        <li>Your name, address, telephone and e-mail contact information; </li>
-                        <li>Identify the copyrighted work and/or trademark claimed to have been infringed; </li>
-                        <li>Identify the content on ROBLOX’s site that you claim infringes your intellectual property;</li>
-                        <li>A description of where the claimed infringing Content is located on our Site (Please provide URLs);  </li>
-                        <li>A statement by you that you have a good faith belief that the disputed use is not 
-                            authorized by the intellectual property owner, its agent, or the law; </li>
-                        <li>A statement by you, made under penalty of perjury, that the above information in your Notice is 
-                            accurate and that you are authorized to act on behalf of the owner of the intellectual property involved. </li>
-                    </ol>
-                </p>
-                ROBLOX's Copyright Agent can be reached at copyright_agent@roblox.com  or P.O. Box 1265, San Mateo, CA 94401, 
+                <b>Copyright Notice</b><br />
+                RBLX.local respects the intellectual property rights of others and we ask our users to do the same. 
+                RBLX.local is a community made project made to revive the old days of ROBLOX.   
+                All of RBLX.local's logos, characters, names, and all related indicia are trademarks of Roblox
+                Corporation, We will take down this project if Roblox wants us to.
             </div>
             <div class="tos-section">
                 <p>
-                    5. If you cheat on ROBLOX games or use cheat programs, including non-ROBLOX software
-                    or programs, to play the games we will freeze your account.
+                    4. If you cheat on RBLX.local games or use cheat programs, including non-RBLX.local software
+                    or programs, to play the games we will terminate your account.
                 </p>
             </div>
             <div class="tos-section">
                 <p>
-                    6. If you create a&nbsp;game, author a model, or&nbsp;write something that catches
-                    our eye on a message board,&nbsp;we might want to use it on the site or elsewhere
-                    when talking about the site. If you're under age 18, remember to <b>ALWAYS</b> check
-                    with your parents before you send anything to us or post anything on the site! By
-                    sending in your content and comments, you (and your parents) are agreeing that it's
-                    okay to repeat on the site and elsewhere what you say, and it's even all right for
-                    us to use it in an ad. So, this means we can use it in any way we want, anywhere,
-                    until the end of time.
-                </p>
-            </div>
-            <div class="tos-section">
-                <p>
-                    7. By using ROBLOX software you agree to allow ROBLOX to collect, use, and store data from your
-                    computer of a non-personal nature such as Operating System, application software and peripheral 
-                    software, in order to identify and address technical and other issues that might occur during its use.
-            </div>
-            <div class="tos-section">
-                <p>
-                    8. Remember, this is a free website and we reserve the right to prohibit the use
+                    5. Remember, this is a free website and we reserve the right to prohibit the use
                     of the site to any user at any time.
                 </p>
             </div>
         </div>
         <p>
-            There are additional rules and guidelines for behavior on ROBLOX beyond the Terms
-            of Service. They can be found in our <a href="http://www.roblox.com/Help/Builderman.aspx?id=221897">
+            There are additional rules and guidelines for behavior on RBLX.local beyond the Terms
+            of Service. They can be found in our <a href="http://www.watr13.icu/Help/Builderman.aspx?id=221897">
                                                      Community Guidelines</a>, and in the rules posted by staff on the Forums.</p>
         <br />
         <center>
-            <font color="red" size="3">Remember - No member of ROBLOX staff will EVER ask you for
+            <font color="red" size="3">Remember - No member of RBLX.local staff will EVER ask you for
                 your password!</font></center>
+        <center>
+            <font color="red" size="3">If you feel like you wanna provide feedback about the website and the development of this project, visit the RBLX.local Github page.</font></center>
     </div>
 </div>
 
