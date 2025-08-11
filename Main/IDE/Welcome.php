461eabf6617fe5083773e3570eb74f4806dcbc18
Skyler <dsatakedown85@gmail.com>
2025-08-11 21:51:08 +0100

    Create Welcome.php

---
+++ Main/IDE/Welcome.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Welcome</title>
+    <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.1.min.js"></script>
+<script type="text/javascript">window.jQuery || document.write("<script type='text/javascript' src='/js/jquery/jquery-1.11.1.js'><\/script>")</script>
+<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.migrate/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript">window.jQuery || document.write("<script type='text/javascript' src='/js/jquery/jquery-migrate-1.2.1.js'><\/script>")</script>
+<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/4.0/1/MicrosoftAjax.js"></script>
+<script type="text/javascript">window.Sys || document.write("<script type='text/javascript' src='/js/Microsoft/MicrosoftAjax.js'><\/script>")</script>
+
+    
+<link rel="stylesheet" href="http://www.watr13.icu/CSS/Base/CSS/page___3b466252a2cd9644b50ab2cc94badcce_m.css"/>
+
+    <script type="text/javascript" src="http://www.watr13.icu/rbxcdn_js/19fc2cf00b071c88456901559592396f.js"></script>
+
+    
+    <script type="text/javascript">Roblox.config.externalResources = [];Roblox.config.paths['Pages.Catalog'] = 'http://js.rbxcdn.com/a2ff3787d1fd8d3c2492b5f5c5ec70b6.js';Roblox.config.paths['Pages.CatalogShared'] = 'http://js.rbxcdn.com/4eb48eec34ca711d5a7b08a4291ac753.js';Roblox.config.paths['Pages.Messages'] = 'http://js.rbxcdn.com/e8cbac58ab4f0d8d4c707700c9f97630.js';Roblox.config.paths['Resources.Messages'] = 'http://js.rbxcdn.com/fb9cb43a34372a004b06425a1c69c9c4.js';Roblox.config.paths['Widgets.AvatarImage'] = 'http://js.rbxcdn.com/bbaeb48f3312bad4626e00c90746ffc0.js';Roblox.config.paths['Widgets.DropdownMenu'] = 'http://js.rbxcdn.com/7b436bae917789c0b84f40fdebd25d97.js';Roblox.config.paths['Widgets.GroupImage'] = 'http://js.rbxcdn.com/33d82b98045d49ec5a1f635d14cc7010.js';Roblox.config.paths['Widgets.HierarchicalDropdown'] = 'http://js.rbxcdn.com/fbb86cf0752d23f389f983419d3085b4.js';Roblox.config.paths['Widgets.ItemImage'] = 'http://js.rbxcdn.com/838ec9c8067ba6fd6793a8bdbdb48a5c.js';Roblox.config.paths['Widgets.PlaceImage'] = 'http://js.rbxcdn.com/f2697119678d0851cfaa6c2270a727ed.js';Roblox.config.paths['Widgets.SurveyModal'] = 'http://js.rbxcdn.com/d6e979598c460090eafb6d38231159f6.js';</script>
+        <script type="text/javascript">
+            function editTemplateInStudio(play_placeId) { RobloxLaunch._GoogleAnalyticsCallback = function() { var isInsideRobloxIDE = 'website'; if (Roblox && Roblox.Client && Roblox.Client.isIDE && Roblox.Client.isIDE()) { isInsideRobloxIDE = 'Studio'; };GoogleAnalyticsEvents.FireEvent(['Edit Location', 'Guest', isInsideRobloxIDE]);GoogleAnalyticsEvents.FireEvent(['Edit', 'Guest', '']); };  Roblox.Client.WaitForRoblox(function() { RobloxLaunch.StartGame('http://www.www.watr13.icu//Game/edit.ashx?PlaceID='+play_placeId+'&upload=', 'edit.ashx', 'http://www.www.watr13.icu//Login/Negotiate.ashx', 'FETCH', true); }); }
+        </script>
+</head>
+<body id="StudioWelcomeBody">
+    <div class="header">
+            <div id="header-login-wrapper" class="iframe-login-signup" data-display-opened="">
+                <a href="http://www.watr13.icu/Login/NewAge.aspx" target="_blank" class="btn-control btn-control-large translate" id="header-signup"><span>Sign Up</span></a>
+                <span id="header-or">or</span>
+                <span class="studioiFrameLogin">
+                    <span id="login-span">
+                        <a id="header-login" class="btn-control btn-control-large">Login <span class="grey-arrow">▼</span></a>
+                    </span>
+
+                    <div id="iFrameLogin" class="studioiFrameLogin" style="display: none">
+                        <iframe class="login-frame" src="http://www.watr13.icu/Login/iFrameLogin.aspx?loginRedirect=True&amp;parentUrl=http%3a%2f%2fwww.www.watr13.icu%2fide%2fwelcome" scrolling="no" frameborder="0"></iframe>
+                    </div>
+                </span>
+            </div>
+        <!-- This is only after the login stuff because IE7 demands floated elements be before non-floated -->
+        <img src="http://www.watr13.icu/images/IDE/img-studio_title.png" alt="Roblox Studio Title"/>
+        <p id="HomeLink">
+            <a class="text-link" href="http://www.watr13.icu/develop">Switch to Classic View</a>
+        </p>
+    </div>
+    <div class="container">
+        <div class="navbar">
+            <ul class="navlist">
+                <li id="NewProject"><p>New Project</p></li>
+                <li id="GamesToggle"><p>Games</p></li>
+                    <li id="MyProjects"><p>My Projects</p></li>
+                                <!--li class="lastnav"><p>Recent News</p></li-->
+            </ul>
+
+        </div>
+        <div class="main">
+            <div id="TemplatesView" class="welcome-content-area">
+
+<h2 id="StudioGameTemplates">GAME TEMPLATES</h2>
+            <div class="templatetypes">
+                <ul class="templatetypes">
+                        <li data-templatetype="Basic"><a href="#Basic">Basic</a></li>
+                        <li data-templatetype="Theme"><a href="#Theme">Theme</a></li>
+                        <li data-templatetype="Gameplay"><a href="#Gameplay">Gameplay</a></li>
+                </ul>
+                <!--div class="tool-tip">
+                    <img alt="Recommended for users new to ROBLOX studio" src="/images/IDE/img-tail-top.png" class="top" />
+                    <p>Recommended for users new to ROBLOX studio</p>
+                    <a class="closeButton"></a>
+                </div -->
+            </div>
+            
+                <div class="templates" data-templatetype="Basic">
+                        <div class="template" placeid="95206881">
+                            <a href="" class="game-image"><img class="" src="http://t1.rbxcdn.com/3cdaedd47bb2d83c0f8fa4d88f15cc08"/></a>
+                            <p>Baseplate</p>
+                        </div>
+                        <div class="template" placeid="95206192">
+                            <a href="" class="game-image"><img class="" src="http://t1.rbxcdn.com/b85d015d5cfb65f342eba95293e35f19"/></a>
+                            <p>Flat Terrain</p>
+                        </div>
+                </div>
+                <div class="templates" data-templatetype="Theme">
+                        <div class="template" placeid="203783329">
+                            <a href="" class="game-image"><img class="" src="http://t0.rbxcdn.com/b5681e74280c69e8111bcde693116de8"/></a>
+                            <p>City</p>
+                        </div>
+                        <div class="template" placeid="203810088">
+                            <a href="" class="game-image"><img class="" src="http://t6.rbxcdn.com/3778aca4a72fc6a836136a9f7c807690"/></a>
+                            <p>Medieval</p>
+                        </div>
+                </div>
+                <div class="templates" data-templatetype="Gameplay">
+                        <div class="template" placeid="203812057">
+                            <a href="" class="game-image"><img class="" src="http://t6.rbxcdn.com/684cafae0c216e9266718a2f615c91ed"/></a>
+                            <p>Obby</p>
+                        </div>
+                        <div class="template" placeid="215383192">
+                            <a href="" class="game-image"><img class="" src="http://t1.rbxcdn.com/3608253a087bddcdc234cca8060a4624"/></a>
+                            <p>Racing</p>
+                        </div>
+                        <div class="template" placeid="203885589">
+                            <a href="" class="game-image"><img class="" src="http://t2.rbxcdn.com/a0804859cfd32ee56a35a9d68bc426d7"/></a>
+                            <p>Shooter</p>
+                        </div>
+                </div>
+
+    <h2 id="latest-articles">LATEST WIKI ARTICLES</h2>
+    <div id="article-container">
+            <div class="item-box">
+                <a href="http://wiki.www.watr13.icu/index.php?title=Joint" title="Joints">
+                    <img class="item-box-image" src="http://wiki.www.watr13.icu/index.php/Special:FilePath/JointExample1.png" data-animated-src="http://wiki.www.watr13.icu/index.php/Special:FilePath/JointExample1.png" data-static-src="http://wiki.www.watr13.icu/index.php/Special:FilePath/JointExample1.png" alt="Joints"/>
+                    <div class="item-box-text">
+                        <p class="article-title">Joints</p>
+                    </div>
+                </a>
+                <p class="article-summary">Learn how the ROBLOX joint system works.</p>
+            </div>
+            <div class="item-box">
+                <a href="http://wiki.www.watr13.icu/index.php?title=Custom_loading_screen" title="Custom Loading Screen">
+                    <img class="item-box-image" src="http://wiki.www.watr13.icu/index.php/Special:FilePath/CustomLoadingScreenStatic.png" data-animated-src="http://wiki.www.watr13.icu/index.php/Special:FilePath/CustomLoadingScreen.gif" data-static-src="http://wiki.www.watr13.icu/index.php/Special:FilePath/CustomLoadingScreenStatic.png" alt="Custom Loading Screen"/>
+                    <div class="item-box-text">
+                        <p class="article-title">Custom Loading Screen</p>
+                    </div>
+                </a>
+                <p class="article-summary">Use ReplicatedFirst to display your own loading screen.</p>
+            </div>
+            <div class="item-box">
+                <a href="http://wiki.www.watr13.icu/index.php?title=Custom_teleport_ui" title="Custom Teleport GUI">
+                    <img class="item-box-image" src="http://wiki.www.watr13.icu/index.php/Special:FilePath/CustomTeleportGUIStatic.png" data-animated-src="http://wiki.www.watr13.icu/index.php/Special:FilePath/CustomTeleportGUI.gif" data-static-src="http://wiki.www.watr13.icu/index.php/Special:FilePath/CustomTeleportGUIStatic.png" alt="Custom Teleport GUI"/>
+                    <div class="item-box-text">
+                        <p class="article-title">Custom Teleport GUI</p>
+                    </div>
+                </a>
+                <p class="article-summary">Replace ROBLOX&#39;s default teleport GUI with your own custom one.</p>
+            </div>
+            <div class="item-box">
+                <a href="http://wiki.www.watr13.icu/index.php?title=Script_analysis" title="Script Analysis">
+                    <img class="item-box-image" src="http://wiki.www.watr13.icu/index.php/Special:FilePath/ScriptAnalysisThumbnail.png" data-animated-src="http://wiki.www.watr13.icu/index.php/Special:FilePath/ScriptAnalysisThumbnail.png" data-static-src="http://wiki.www.watr13.icu/index.php/Special:FilePath/ScriptAnalysisThumbnail.png" alt="Script Analysis"/>
+                    <div class="item-box-text">
+                        <p class="article-title">Script Analysis</p>
+                    </div>
+                </a>
+                <p class="article-summary">Learn how to use the Script Analysis tool in Studio to check your code.</p>
+            </div>
+            <div class="item-box">
+                <a href="http://wiki.www.watr13.icu/index.php?title=Solid_modeling" title="Solid Modeling">
+                    <img class="item-box-image" src="http://wiki.www.watr13.icu/index.php/Special:FilePath/SolidModelingSample1.png" data-animated-src="http://wiki.www.watr13.icu/index.php/Special:FilePath/Negate.gif" data-static-src="http://wiki.www.watr13.icu/index.php/Special:FilePath/SolidModelingSample1.png" alt="Solid Modeling"/>
+                    <div class="item-box-text">
+                        <p class="article-title">Solid Modeling</p>
+                    </div>
+                </a>
+                <p class="article-summary">Learn how to use Solid Modeling to create custom shaped parts.</p>
+            </div>
+    </div>
+            </div>
+            <div id="MyProjectsView" class="welcome-content-area" style="display: none">
+                <div>
+                        <h2>My Places</h2>
+                    <div id="assetList" class="tab-active">
+
+     
+    <div>
+        <span>You must be logged in to view your published projects!</span>
+    </div>
+    <script type="text/javascript">
+        $('#MyProjects').click(function() {
+            $('#header-login').addClass('active');
+            $('#iFrameLogin').css('display', 'block');
+        });
+    </script>
+
+                    </div>
+                </div>
+            </div>
+            <div id="GamesView" class="welcome-content-area" style="display: none">
+                <div>
+                        <h2>My Games</h2>
+                    <div id="universeList" class="tab-active">
+
+     
+    <div>
+        <span>You must be logged in to view your published projects!</span>
+    </div>
+    <script type="text/javascript">
+        $('#MyProjects').click(function () {
+            $('#header-login').addClass('active');
+            $('#iFrameLogin').css('display', 'block');
+        });
+    </script>
+
+                    </div>
+                </div>
+            </div>
+
+            <div id="ButtonRow" class="divider-top divider-left divider-bottom">
+                <a class="btn-medium btn-primary" id="EditButton">Edit</a>
+                <a class="btn-medium btn-primary" id="BuildButton">Build</a>
+                <a class="btn-medium btn-negative" id="CollapseButton">Cancel</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="GenericModal modalPopup unifiedModal smallModal" style="display:none;">
+        <div class="Title"></div>
+        <div class="GenericModalBody">
+            <div>
+                <div class="ImageContainer">
+                    <img class="GenericModalImage" alt="generic image"/>
+                </div>
+                <div class="Message"></div>
+            </div>
+            <div class="clear"></div>
+            <div id="GenericModalButtonContainer" class="GenericModalButtonContainer">
+                <a class="ImageButton btn-neutral btn-large roblox-ok">OK<span class="btn-text">OK</span></a>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        $(function () {
+            if (typeof Roblox.IDEWelcome === "undefined")
+                Roblox.IDEWelcome = {};
+
+            Roblox.IDEWelcome.Resources = {
+                //<sl:translate>
+                openProject: "Open Project",
+                openProjectText: "To open your project, open to this page in ",
+                robloxStudio: "ROBLOX Studio",
+                editPlace: "Edit Place",
+                toEdit: "To edit ",
+                openPage: ", open to this page in ",
+                buildPlace: "Build Place",
+                toBuild: "To build on ",
+                placeInactive: "Place Inactive",
+                activate: ", activate this place by going to File->My Published Projects.",
+                emailVerifiedTitle: "Verify Your Email",
+                emailVerifiedMessage: "You must verify your email before you can work on your place. You can verify your email on the <a href='/my/account?confirmemail=1'>Account</a> page.",
+                verify: "Verify",
+                OK: "OK"
+                //</sl:translate>
+            };
+        });
+    </script>
+    
+
+<div class="ConfirmationModal modalPopup unifiedModal smallModal" data-modal-handle="confirmation" style="display:none;">
+    <a class="genericmodal-close ImageButton closeBtnCircle_20h"></a>
+    <div class="Title"></div>
+    <div class="GenericModalBody">
+        <div class="TopBody">
+            <div class="ImageContainer roblox-item-image" data-image-size="small" data-no-overlays data-no-click>
+                <img class="GenericModalImage" alt="generic image"/>
+            </div>
+            <div class="Message"></div>
+        </div>
+        <div class="ConfirmationModalButtonContainer GenericModalButtonContainer">
+            <a href id="roblox-confirm-btn"><span></span></a>
+            <a href id="roblox-decline-btn"><span></span></a>
+        </div>
+        <div class="ConfirmationModalFooter">
+        
+        </div>  
+    </div>  
+    <script type="text/javascript">
+        Roblox = Roblox || {};
+        Roblox.Resources = Roblox.Resources || {};
+        
+        //<sl:translate>
+        Roblox.Resources.GenericConfirmation = {
+            yes: "Yes",
+            No: "No",
+            Confirm: "Confirm",
+            Cancel: "Cancel"
+        };
+        //</sl:translate>
+    </script>
+</div>
+
+
+    <script type="text/javascript">function urchinTracker() {}</script>
+
+</body>
+</html>
