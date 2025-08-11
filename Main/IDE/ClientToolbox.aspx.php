8001ba417ce1ef5777e7214d824a9509ced6df30
Skyler <dsatakedown85@gmail.com>
2025-08-11 21:52:37 +0100

    Create ClientToolbox.aspx.php

---
+++ Main/IDE/ClientToolbox.aspx.php
@@ -0,0 +1,120 @@
+
+<head><title>Toolbox</title>
+		 
+<link rel="stylesheet" href="http://www.watr13.icu/CSS/Base/CSS/page___b05f9fa8b62e0f38b935a5fc2411d93b_m.css">
+
+		 <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.1.min.js"></script>
+<script type="text/javascript">window.jQuery || document.write("<script type='text/javascript' src='/js/jquery/jquery-1.11.1.js'><\/script>")</script>
+<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.migrate/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript">window.jQuery || document.write("<script type='text/javascript' src='/js/jquery/jquery-migrate-1.2.1.js'><\/script>")</script>
+
+		 <script type="text/javascript" src="http://www.watr13.icu/rbxcdn_js/be45a84247f84dd8b4ffe764c65135c7.js"></script>
+		 <script>
+		 		    function clickButton(e, buttonid)
+		    {
+			    var bt = document.getElementById(buttonid);
+			    if (typeof bt == 'object')
+			    {
+				    if(navigator.appName.indexOf("Netscape")>(-1))
+				    {
+					    if (e.keyCode == 13)
+					    {
+						    bt.click();
+						    return false;
+					    }
+				    }
+				    if (navigator.appName.indexOf("Microsoft Internet Explorer")>(-1))
+				    {
+					    if (event.keyCode == 13)
+					    {
+						    bt.click();
+						    return false;
+					    }
+				    }
+			    }
+		    }
+        </script>
+
+	</head>
+<body class="Page" style="margin: 0;">
+        <input name="__RequestVerificationToken" type="hidden" value="6_eZHOjUPq8Jhw66Ug0so8DxlG33_rZY0TrLaXEc7aMbOKqRbphTsdZWYh_pBl5ud60toqWtjSAZmQHQU93ZLxukFYLaUIRjFnWCQD57CiwhlECKHNRU2ejI5FDEhDWcZ1Ru3g2">
+        <div id="NewToolboxContainer" data-isuserauthenticated="false" data-isdecalcreationenabled="true" data-requesturl="http://www.watr13.icu/asset/" data-isrecentlyinsertedassetenabled="true">
+            <div id="ToolboxControls">
+                <div id="SetTabs">
+                    <div id="Inventory" class="Tabs">Inventory</div>
+                    <div id="Search" class="Tabs Selected">Search</div>
+                </div>
+                <div id="ToolboxSelector">
+                    <span class="toolboxDisplayText" style="display: none;">Display: </span>
+                    <select id="ddlSets" class="Toolboxes" style="min-width: 135px; display: none;"><optgroup label="ROBLOX Sets"></optgroup></select>
+                    <div id="SearchList" class="SetList SetOptions hidden" style="float: left; min-width: 58px; display: block;">
+                        <a href="#" id="activeOption" class="btn-dropdown" data-value="FreeModels">Models</a>
+                        <span class="dropdownImg"></span>
+                        <div class="clear"></div>
+                        <div class="SetListDropDown">
+                            <div class="SetListDropDownList invisible">
+                                <div id="SearchMenu" class="menu invisible"><a href="#" data-value="FreeModels" class="searchListOption">Models</a><a href="#" data-value="FreeDecals" class="searchListOption">Decals</a></div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="ToolboxSearch" class="hidden" style="display: block;">
+                        <input type="text" id="tbSearch" title="Search" class="Search greyText">
+                        <div id="Button" class="ButtonText"></div>
+                    </div>
+                </div>
+            </div>
+            <div id="ToolBoxScrollWrapper" style="height: 592px;">
+                <div id="Filters" class="searchFilter hidden" style="display: block;">
+                    <span class="filterText">Sort by: </span>
+                    <select name="SortList" id="SortList" class="Toolboxes" style="float:none;min-width: 103px">
+                        <option value="Relevance">Relevance</option>
+                        <option value="MostTaken">Most Taken</option>
+                        <option value="Favorites">Favorites</option>
+                        <option value="Updated">Updated</option>
+                    </select>
+                </div>
+                <div id="ResultStatus" class="hidden" style="display: none;"></div>
+                <div id="Navigation" class="Navigation hidden">
+                </div>
+                <div id="ToolboxItems"><div class="ToolboxItem WithoutVoting" id="span_setitem_464140" ondragstart="dragRBX(464140, false)">   <a href="javascript:insertContent(464140)" class="itemLink" title="Game Stuff">       <img alt="Game Stuff" id="img_span_setitem_464140" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_464138" ondragstart="dragRBX(464138, false)">   <a href="javascript:insertContent(464138)" class="itemLink" title="Baseplates">       <img alt="Baseplates" id="img_span_setitem_464138" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_464131" ondragstart="dragRBX(464131, false)">   <a href="javascript:insertContent(464131)" class="itemLink" title="Weapons">       <img alt="Weapons" id="img_span_setitem_464131" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_463266" ondragstart="dragRBX(463266, false)">   <a href="javascript:insertContent(463266)" class="itemLink" title="Vehicles">       <img alt="Vehicles" id="img_span_setitem_463266" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_458339" ondragstart="dragRBX(458339, false)">   <a href="javascript:insertContent(458339)" class="itemLink" title="Bricks">       <img alt="Bricks" id="img_span_setitem_458339" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_360380" ondragstart="dragRBX(360380, false)">   <a href="javascript:insertContent(360380)" class="itemLink" title="Basic Building">       <img alt="Basic Building" id="img_span_setitem_360380" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_360378" ondragstart="dragRBX(360378, false)">   <a href="javascript:insertContent(360378)" class="itemLink" title="Advanced Building">       <img alt="Advanced Building" id="img_span_setitem_360378" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_360375" ondragstart="dragRBX(360375, false)">   <a href="javascript:insertContent(360375)" class="itemLink" title="House Kit">       <img alt="House Kit" id="img_span_setitem_360375" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_360372" ondragstart="dragRBX(360372, false)">   <a href="javascript:insertContent(360372)" class="itemLink" title="House Interior Kit">       <img alt="House Interior Kit" id="img_span_setitem_360372" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_360371" ondragstart="dragRBX(360371, false)">   <a href="javascript:insertContent(360371)" class="itemLink" title="Landscape">       <img alt="Landscape" id="img_span_setitem_360371" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_360369" ondragstart="dragRBX(360369, false)">   <a href="javascript:insertContent(360369)" class="itemLink" title="Castle Kit">       <img alt="Castle Kit" id="img_span_setitem_360369" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_360365" ondragstart="dragRBX(360365, false)">   <a href="javascript:insertContent(360365)" class="itemLink" title="Castle Interior Kit">       <img alt="Castle Interior Kit" id="img_span_setitem_360365" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_360363" ondragstart="dragRBX(360363, false)">   <a href="javascript:insertContent(360363)" class="itemLink" title="Space Kit">       <img alt="Space Kit" id="img_span_setitem_360363" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_360362" ondragstart="dragRBX(360362, false)">   <a href="javascript:insertContent(360362)" class="itemLink" title="Fun Machines">       <img alt="Fun Machines" id="img_span_setitem_360362" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div><div class="ToolboxItem WithoutVoting" id="span_setitem_360360" ondragstart="dragRBX(360360, false)">   <a href="javascript:insertContent(360360)" class="itemLink" title="Deadly Machines">       <img alt="Deadly Machines" id="img_span_setitem_360360" src="/thumbs/unavailable.jpg" border="0px" style="height:62px;width:60px;" onerror="return Roblox.Controls.Image.OnError(this)">   </a></div></div>
+                <div id="ShowMore" class="Navigation hidden" style="clear: both; display: none;">
+                    <div id="showMoreNext">
+                        <a id="showMoreButton" class="btn-control btn-control-small" style="cursor:pointer;margin-left: 2px;">Show More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script type="text/javascript">
+            if (typeof ClientToolbox === "undefined") {
+                ClientToolbox = {};
+            }
+
+            ClientToolbox.Resources = {
+                //<sl:translate>
+                models: "Models",
+                recentModels: "Recent models",
+                recentDecals: "Recent decals",
+                myModels: "My Models",
+                myDecals: "My Decals",
+                decals: "Decals",
+                mySets: "My Sets",
+                mySubscribedSets: "My Subscribed Sets",
+                robloxSets: "ROBLOX Sets",
+                noSets: "No sets are available",
+                setsError: "An error occured while retrieving sets",
+                results: "Results",
+                loading: "Loading...",
+                noResults: "No results found.",
+                error: "Error Occurred.",
+                errorData: "An error occured while retrieving this data",
+                insertError: "Could not insert the requested item",
+                dragError: "Sorry Could not drag the requested item",
+                noVotesYet: "No votes yet",
+                endorsedAsset: "A high-quality item",
+                //</sl:translate>
+                endorsedIcon: "http://www.watr13.icu/rbxcdn_img/a98989e47370589a088675aaca5eaab8.png"
+            };
+        </script>
+	
+</body>
