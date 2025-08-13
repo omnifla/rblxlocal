9d9800a5d3568293a87e031c3b754cd18437d4e7
TheGuyWhoIsIdiot <157973030+TheGuyWhoIsIdiot@users.noreply.github.com>
2025-08-13 15:17:21 -0300

    Added Send.ashx.php

---
+++ Main/Chat/Send.ashx.php
@@ -0,0 +1,26 @@
+<?php
+// written by meditext
+// stub api, lets just use this for now
+// this will be replaced eventually to use Roblox.Platform.Chat system.
+require_once $_SERVER["DOCUMENT_ROOT"] . '/../config/main.php';
+use Roblox\Authentication as Auth;
+use Roblox\TextFilter\BasicTextFilter;
+header('Content-Type:application/json');
+
+$recipient = $_GET["recipientUserId"];
+$chatData = [
+	"Error" => null,
+];
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    $chatData['Error'] = "Invalid request";
+    exit(json_encode($chatData));
+}
+$message = $_POST["message"];
+if(!Auth::GetUserInfo(intval($recipient))){
+    $chatData['Error'] = "Recipient user not found";
+    exit(json_encode($chatData));
+}
+$filter_test = new BasicTextFilter();
+$message = $filter_test->filterText($_POST["message"], Auth::GetCurrentUserId(), intval($recipient), $message);
+exit(json_encode($chatData)); // stub
+?>
