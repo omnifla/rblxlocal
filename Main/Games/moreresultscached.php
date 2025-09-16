c7422f0967aa2ce9ebc3f54e6af52b42b40dc4b0
Skyler <dsatakedown85@gmail.com>
2025-09-16 15:24:22 +0100

    Update moreresultscached.php

--- Main/Games/moreresultscached.php
+++ Main/Games/moreresultscached.php
@@ -1,1 +1,48 @@
-Games will be worked on soon.
+<li class="list-item game">
+    <a href="/games/refer?SortFilter=16&amp;TimeFilter=2&amp;GenreFilter=1&amp;PlaceId=1&amp;Position=15&amp;PageType=Games" class="game-item">
+        <span class="game-thumb-content">
+            <span class="game-thumb-wrapper"
+                  >
+                <img class="game-thumb" src="https://thumbs.aftwld.xyz/edc72a454ded555713243c50f306aca3.png" alt="Soon..."
+                     thumbnail='{"Final":true,"Url":"https://thumbs.aftwld.xyz/edc72a454ded555713243c50f306aca3.png","RetryUrl":null}' image-retry />
+            </span>
+        </span>
+        <span class="rbx-game-title rbx-text-overflow" title="Soon...">
+            Soon...
+        </span>
+        <span class="rbx-game-text-notes rbx-font-xs">
+            NaN Players Online
+        </span>
+        <span class="rbx-votes">
+            <div class="vote-bar">
+                <div class="thumbs-up">
+                    <span class="rbx-icon-thumbs-up"></span>
+                </div>
+                <div class="voting-container"
+                     data-upvotes="1335"
+                     data-downvotes="851"
+                     data-voting-processed="false">
+                    <div class="background "></div>
+                    <div class="votes"></div>
+                    <div class="mask">
+                        <div class="segment seg-one"></div>
+                        <div class="segment seg-two"></div>
+                        <div class="segment seg-three"></div>
+                        <div class="segment seg-four"></div>
+                    </div>
+                </div>
+                <div class="thumbs-down">
+                    <span class="rbx-icon-thumbs-down"></span>
+                </div>
+            </div>
+            <div class="vote-counts">
+                <div class="down-votes-count rbx-font-xs">67</div>
+                <div class="up-votes-count rbx-font-xs">67</div>
+
+            </div>
+        </span>
+        <span class="rbx-developer rbx-font-xs">
+            by <cite class="rbx-link-sm" data-href="/User.aspx?ID=3">NeuroSama</cite>
+        </span>
+    </a>
+</li>
