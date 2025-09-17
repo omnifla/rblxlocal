f95807ce164afc4652c15935eebfb7a275985d08
Skyler <dsatakedown85@gmail.com>
2025-09-17 12:59:56 +0100

    Create test.php

---
+++ Mobile/test.php
@@ -0,0 +1,264 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head><script src="//archive.org/includes/athena.js" type="text/javascript"></script>
+<script type="text/javascript">window.addEventListener('DOMContentLoaded',function(){var v=archive_analytics.values;v.service='wb';v.server_name='wwwb-app225.us.archive.org';v.server_ms=491;archive_analytics.send_pageview({});});</script>
+<script type="text/javascript" src="https://web-static.archive.org/_static/js/bundle-playback.js?v=FC38Hc5A" charset="utf-8"></script>
+<script type="text/javascript" src="https://web-static.archive.org/_static/js/wombat.js?v=txqj7nKC" charset="utf-8"></script>
+<script>window.RufflePlayer=window.RufflePlayer||{};window.RufflePlayer.config={"autoplay":"on","unmuteOverlay":"hidden","showSwfDownload":true};</script>
+<script type="text/javascript" src="https://web-static.archive.org/_static/js/ruffle/ruffle.js"></script>
+<script type="text/javascript">
+    __wm.init("https://web.archive.org/web");
+  __wm.wombat("https://m.roblox.com/Login?ReturnUrl=%2F","20150312214338","https://web.archive.org/","web","https://web-static.archive.org/_static/",
+	      "1426196618");
+</script>
+<link rel="stylesheet" type="text/css" href="https://web-static.archive.org/_static/css/banner-styles.css?v=p7PEIJWi" />
+<link rel="stylesheet" type="text/css" href="https://web-static.archive.org/_static/css/iconochive.css?v=3PDvdIFv" />
+<!-- End Wayback Rewrite JS Include -->
+
+                <title>ROBLOX - Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/> 
+        <link rel="shortcut icon" href="/web/20150312214338im_/https://m.roblox.com/favicon.ico"/>
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/web/20150312214338im_/https://m.roblox.com/Images/Icon114x114v3.png">
+
+<link rel="stylesheet" href="/web/20150312214338cs_/https://m.roblox.com/CSS/Base/CSS/FetchCSS?path=page___c1f266fd667554c60776af47cca6a8ab_m.css"/>
+        <script type="text/javascript" src="//web.archive.org/web/20150312214338js_/https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.2.min.js"></script>
+        <script type="text/javascript" src="//web.archive.org/web/20150312214338js_/https://ajax.aspnetcdn.com/ajax/jquery.mobile/1.1.1/jquery.mobile-1.1.1.min.js"></script>
+<script type="text/javascript" src="https://web.archive.org/web/20150312214338js_/https://s3.amazonaws.com/js.roblox.com/ba00b3edd1e67e9348a47a67a08fa69e.js"></script>
+    <script type="text/javascript" src="https://web.archive.org/web/20150312214338js_/http://cdn.gigya.com/js/gigya.js?apiKey=3_OsvmtBbTg6S_EUbwTPtbbmoihFY5ON6v6hbVrTbuqpBs7SyF_LQaJwtwKJ60sY1p"></script>
+
+        
+    </head>
+    <body data-ga-key="UA-486632-9" data-ga-devicename="Unknown"><!-- BEGIN WAYBACK TOOLBAR INSERT -->
+<script>__wm.rw(0);</script>
+<div id="wm-ipp-base" lang="en" style="display:none;direction:ltr;" toolbar-mode="auto">
+<div id="wm-ipp" style="position:fixed;left:0;top:0;right:0;">
+<div id="donato" style="position:relative;width:100%;height:0;">
+  <div id="donato-base">
+    <iframe id="donato-if" src="https://archive.org/includes/donate.php?as_page=1&amp;platform=wb&amp;referer=https%3A//web.archive.org/web/20150312214338/https%3A//m.roblox.com/Login%3FReturnUrl%3D%252F"
+	    scrolling="no" frameborder="0" style="width:100%; height:100%">
+    </iframe>
+  </div>
+</div><div id="wm-ipp-inside">
+  <div id="wm-toolbar" style="position:relative;display:flex;flex-flow:row nowrap;justify-content:space-between;">
+    <div id="wm-logo" style="/*width:110px;*/padding-top:12px;">
+      <a href="/web/" title="Wayback Machine home page"><img src="https://web-static.archive.org/_static/images/toolbar/wayback-toolbar-logo-200.png" srcset="https://web-static.archive.org/_static/images/toolbar/wayback-toolbar-logo-100.png, https://web-static.archive.org/_static/images/toolbar/wayback-toolbar-logo-150.png 1.5x, https://web-static.archive.org/_static/images/toolbar/wayback-toolbar-logo-200.png 2x" alt="Wayback Machine" style="width:100px" border="0" /></a>
+    </div>
+    <div class="c" style="display:flex;flex-flow:column nowrap;justify-content:space-between;flex:1;">
+      <form class="u" style="display:flex;flex-direction:row;flex-wrap:nowrap;" target="_top" method="get" action="/web/submit" name="wmtb" id="wmtb"><input type="text" name="url" id="wmtbURL" value="https://m.roblox.com/Login?ReturnUrl=%2F" onfocus="this.focus();this.select();" style="flex:1;"/><input type="hidden" name="type" value="replay" /><input type="hidden" name="date" value="20150312214338" /><input type="submit" value="Go" />
+      </form>
+      <div style="display:flex;flex-flow:row nowrap;align-items:flex-end;">
+                <div class="s" id="wm-nav-captures" style="flex:1;">
+                    <a class="t" href="/web/20150312214338*/https://m.roblox.com/Login?ReturnUrl=%2F" title="See a list of every capture for this URL">77 captures</a>
+          <div class="r" title="Timespan for captures of this URL">26 Aug 2012 - 08 Apr 2025</div>
+          </div>
+        <div class="k">
+          <a href="" id="wm-graph-anchor">
+            <div id="wm-ipp-sparkline" title="Explore captures for this URL" style="position: relative">
+              <canvas id="wm-sparkline-canvas" width="750" height="27" border="0"></canvas>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="n">
+      <table>
+        <tbody>
+          <!-- NEXT/PREV MONTH NAV AND MONTH INDICATOR -->
+          <tr class="m">
+            <td class="b" nowrap="nowrap"><a href="https://web.archive.org/web/20150208115946/http://m.roblox.com:80/Login?ReturnUrl=%2F" title="08 Feb 2015"><strong>Feb</strong></a></td>
+            <td class="c" id="displayMonthEl" title="You are here: 21:43:38 Mar 12, 2015">MAR</td>
+            <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20150415025045/http://m.roblox.com/Login?ReturnUrl=%2f" title="15 Apr 2015"><strong>Apr</strong></a></td>
+          </tr>
+          <!-- NEXT/PREV CAPTURE NAV AND DAY OF MONTH INDICATOR -->
+          <tr class="d">
+            <td class="b" nowrap="nowrap"><a href="https://web.archive.org/web/20150310164404/http://m.roblox.com:80/Login?ReturnUrl=%2F" title="16:44:04 Mar 10, 2015"><img src="https://web-static.archive.org/_static/images/toolbar/wm_tb_prv_on.png" alt="Previous capture" width="14" height="16" border="0" /></a></td>
+            <td class="c" id="displayDayEl" style="width:34px;font-size:22px;white-space:nowrap;" title="You are here: 21:43:38 Mar 12, 2015">12</td>
+            <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20150320025655/http://m.roblox.com/Login?ReturnUrl=%2f" title="02:56:55 Mar 20, 2015"><img src="https://web-static.archive.org/_static/images/toolbar/wm_tb_nxt_on.png" alt="Next capture" width="14" height="16" border="0" /></a></td>
+          </tr>
+          <!-- NEXT/PREV YEAR NAV AND YEAR INDICATOR -->
+          <tr class="y">
+            <td class="b" nowrap="nowrap"><a href="https://web.archive.org/web/20140218112657/http://m.roblox.com:80/Login?ReturnUrl=%2F" title="18 Feb 2014"><strong>2014</strong></a></td>
+            <td class="c" id="displayYearEl" title="You are here: 21:43:38 Mar 12, 2015">2015</td>
+            <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20170123174048/http://m.roblox.com/Login?ReturnUrl=%2F" title="23 Jan 2017"><strong>2017</strong></a></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="r" style="display:flex;flex-flow:column nowrap;align-items:flex-end;justify-content:space-between;">
+      <div id="wm-btns" style="text-align:right;height:23px;">
+                <span class="xxs">
+          <div id="wm-save-snapshot-success">success</div>
+          <div id="wm-save-snapshot-fail">fail</div>
+          <a id="wm-save-snapshot-open" href="#" title="Share via My Web Archive" >
+            <span class="iconochive-web"></span>
+          </a>
+          <a href="https://archive.org/account/login.php" title="Sign In" id="wm-sign-in">
+            <span class="iconochive-person"></span>
+          </a>
+          <span id="wm-save-snapshot-in-progress" class="iconochive-web"></span>
+        </span>
+                <a class="xxs" href="https://help.archive.org/help/category/the-wayback-machine/" title="Get some help using the Wayback Machine" style="top:-6px;"><span class="iconochive-question" style="color:rgb(87,186,244);font-size:160%;"></span></a>
+        <a id="wm-tb-close" href="#close" style="top:-2px;" title="Close the toolbar"><span class="iconochive-remove-circle" style="color:#888888;font-size:240%;"></span></a>
+      </div>
+      <div id="wm-share" class="xxs">
+        <a href="/web/20150312214338/http://web.archive.org/screenshot/https://m.roblox.com/Login?ReturnUrl=%2F"
+           id="wm-screenshot"
+           title="screenshot">
+          <span class="wm-icon-screen-shot"></span>
+        </a>
+        <a href="#" id="wm-video" title="video">
+          <span class="iconochive-movies"></span>
+        </a>
+        <a id="wm-share-facebook" href="#" data-url="https://web.archive.org/web/20150312214338/https://m.roblox.com/Login?ReturnUrl=%2F" title="Share on Facebook" style="margin-right:5px;" target="_blank"><span class="iconochive-facebook" style="color:#3b5998;font-size:160%;"></span></a>
+        <a id="wm-share-twitter" href="#" data-url="https://web.archive.org/web/20150312214338/https://m.roblox.com/Login?ReturnUrl=%2F" title="Share on Twitter" style="margin-right:5px;" target="_blank"><span class="iconochive-twitter" style="color:#1dcaff;font-size:160%;"></span></a>
+      </div>
+      <div style="padding-right:2px;text-align:right;white-space:nowrap;">
+        <a id="wm-expand" class="wm-btn wm-closed" href="#expand"><span id="wm-expand-icon" class="iconochive-down-solid"></span> <span class="xxs" style="font-size:80%;">About this capture</span></a>
+      </div>
+    </div>
+  </div>
+    <div id="wm-capinfo" style="border-top:1px solid #777;display:none; overflow: hidden">
+        <div id="wm-capinfo-notice" source="api"></div>
+                <div id="wm-capinfo-collected-by">
+    <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center;padding:2px 0;">COLLECTED BY</div>
+    <div style="padding:3px;position:relative" id="wm-collected-by-content">
+            <div style="display:inline-block;vertical-align:top;width:50%;">
+			<span class="c-logo" style="background-image:url(https://archive.org/services/img/webwidecrawl);"></span>
+		Organization: <a style="color:#33f;" href="https://archive.org/details/webwidecrawl" target="_new"><span class="wm-title">Internet Archive</span></a>
+		<div style="max-height:75px;overflow:hidden;position:relative;">
+	  <div style="position:absolute;top:0;left:0;width:100%;height:75px;background:linear-gradient(to bottom,rgba(255,255,255,0) 0%,rgba(255,255,255,0) 90%,rgba(255,255,255,255) 100%);"></div>
+	  The Internet Archive discovers and captures web pages through many different web crawls.
+
+At any given time several distinct crawls are running, some for months, and some every day or longer.
+
+View the web archive through the <a href="http://archive.org/web/web.php">Wayback Machine</a>.
+	</div>
+	      </div>
+      <div style="display:inline-block;vertical-align:top;width:49%;">
+			<span class="c-logo" style="background-image:url(https://archive.org/services/img/liveweb)"></span>
+		<div>Collection: <a style="color:#33f;" href="https://archive.org/details/liveweb" target="_new"><span class="wm-title">Live Web Proxy Crawls</span></a></div>
+		<div style="max-height:75px;overflow:hidden;position:relative;">
+	  <div style="position:absolute;top:0;left:0;width:100%;height:75px;background:linear-gradient(to bottom,rgba(255,255,255,0) 0%,rgba(255,255,255,0) 90%,rgba(255,255,255,255) 100%);"></div>
+	  Content crawled via the <a href="http://archive.org/web/web.php">Wayback Machine</a> Live Proxy mostly by the Save Page Now feature on web.archive.org.
+<br><br>
+Liveweb proxy is a component of Internet Archive’s wayback machine project. The liveweb proxy captures the content of a web page in real time, archives it into a ARC or WARC file and returns the ARC/WARC record back to the wayback machine to process. The recorded ARC/WARC file becomes part of the wayback machine in due course of time.
+<br>
+	</div>
+	      </div>
+    </div>
+    </div>
+    <div id="wm-capinfo-timestamps">
+    <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center;padding:2px 0;" title="Timestamps for the elements of this page">TIMESTAMPS</div>
+    <div>
+      <div id="wm-capresources" style="margin:0 5px 5px 5px;max-height:250px;overflow-y:scroll !important"></div>
+      <div id="wm-capresources-loading" style="text-align:left;margin:0 20px 5px 5px;display:none"><img src="https://web-static.archive.org/_static/images/loading.gif" alt="loading" /></div>
+    </div>
+    </div>
+  </div></div></div></div><div id="wm-ipp-print">The Wayback Machine - https://web.archive.org/web/20150312214338/https://m.roblox.com/Login?ReturnUrl=%2F</div>
+<script type="text/javascript">//<![CDATA[
+__wm.bt(750,27,25,2,"web","https://m.roblox.com/Login?ReturnUrl=%2F","20150312214338",1996,"https://web-static.archive.org/_static/",["https://web-static.archive.org/_static/css/banner-styles.css?v=p7PEIJWi","https://web-static.archive.org/_static/css/iconochive.css?v=3PDvdIFv"], false);
+  __wm.rw(1);
+//]]></script>
+<!-- END WAYBACK TOOLBAR INSERT -->
+ 
+        <div id="login-page" data-role="page">
+                <div class="wrapper">
+                    <div data-role="header" data-id="header">
+                        <div class="header-icons header-icons-left">
+                            <a href="#" data-show-menu-link class="header-icons-menu"></a>
+                        </div>
+                            <h1 class="header-logo-only"></h1>
+                                            </div>                                       
+                    <div data-role="content">
+                        
+
+<form action="/web/20150312214338/https://m.roblox.com/login" data-ajax="false" id="LogOnForm" method="post">    <p>
+        Log in to ROBLOX mobile to keep in touch with your friends.
+    </p>
+    <hr/>
+    <div id="CredentialsSection">
+        <input data-val="true" data-val-required="The User name field is required." id="UserName" name="UserName" placeholder="Username" type="text" value=""/>
+        <input data-val="true" data-val-required="The Password field is required." id="Password" name="Password" placeholder="Password" type="password"/>
+
+        <div class="ui-grid-a no-margin-grid">
+            <div class="ui-block-a">
+                <button type="submit" id="LogInButton" data-theme="b" data-use-sign-on-api="False" data-sign-on-api-path="https://api.roblox.com/login/v1">
+                    Log In
+                </button>
+            </div>
+            <div class="ui-block-b">
+                <a data-role="button" data-theme="f" data-ajax="false" href="/web/20150312214338/https://m.roblox.com/Account/SignUp">Sign Up</a>
+            </div>
+        </div>
+    </div>
+    <div id="TwoStepVerificationSection" style="display:none" data-request-code-unauthenticated-path="https://api.roblox.com/twostepverification/request-unauthenticated" data-verify-code-unauthenticated-path="https://api.roblox.com/twostepverification/verify-unauthenticated">
+        <div id="TwoStepVerificatonMessage">Enter your two step verification code.</div>
+        <input id="IdentificationCode" name="IdentificationCode" placeholder="Code" type="text" value=""/>
+
+        <div class="ui-grid-a no-margin-grid">
+            <div class="ui-block-a" id="SubmitCodeButtonBlock">
+                <button id="SubmitCodeButton" data-theme="b">
+                    Submit
+                </button>
+            </div>
+            <div class="ui-block-a" id="GenerateNewCodeButtonBlock" style="display:none">
+                <button id="GenerateNewCodeButton" data-theme="b">
+                    New Code
+                </button>
+            </div>
+            <div class="ui-block-b" id="CancelCodeButtonBlock">
+                <button id="CancelCodeButton" data-theme="f">
+                    Cancel
+                </button>
+            </div>
+        </div>
+    </div>
+</form>
+<p style="text-align: center;">
+    <a data-rel="external" href="https://web.archive.org/web/20150312214338/http://www.roblox.com/Default.aspx?mobile=false">View Full Site</a><br/><br/>
+</p>
+<p style="text-align: center;">By clicking Log In, you agree to our <a href="https://web.archive.org/web/20150312214338/http://www.roblox.com/info/terms-of-service">Terms of Service</a>, <a href="https://web.archive.org/web/20150312214338/http://www.roblox.com/info/eula.htm">Licensing Agreement</a>, and <a href="https://web.archive.org/web/20150312214338/http://www.roblox.com/Info/Privacy.aspx">Privacy Policy</a>.</p>
+                    </div>
+                </div>
+        </div>
+        
+            <div id="menu">
+                                    <ul class="menu-first"> <li><a href="/web/20150312214338/https://m.roblox.com/login">Home</a></li> </ul>                  <h3>Explore</h3>
+                <ul>
+                     <li><a href="https://web.archive.org/web/20150312214338/http://m.roblox.com/people">People</a></li> 
+                     <li><a href="https://web.archive.org/web/20150312214338/http://m.roblox.com/my-groups">Groups</a></li> 
+                     <li><a href="https://web.archive.org/web/20150312214338/http://m.roblox.com/catalog">Catalog</a></li> 
+                </ul>
+                <h3>&nbsp;</h3>
+                <ul>                                        
+                     <li><a href="https://web.archive.org/web/20150312214338/http://www.roblox.com/Default.aspx?mobile=false">Full Site</a></li> 
+                     <li><a href="https://web.archive.org/web/20150312214338/http://blog.roblox.com/">Blog</a></li> 
+                     <li><a href="https://web.archive.org/web/20150312214338/http://www.roblox.com/info/terms-of-service">Terms of Service</a></li> 
+                     <li><a href="https://web.archive.org/web/20150312214338/http://www.roblox.com/Info/Privacy.aspx">Privacy Policy</a></li> 
+                    <li class="menu-footer"><p>ROBLOX &copy; 2015</p></li>
+                </ul>
+            </div>
+            <div id="screen-cover"></div>
+    </body>
+</html><!--
+     FILE ARCHIVED ON 21:43:38 Mar 12, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 11:48:52 Sep 17, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+-->
+<!--
+playback timings (ms):
+  captures_list: 1.336
+  exclusion.robots: 0.031
+  exclusion.robots.policy: 0.021
+  esindex: 0.01
+  cdx.remote: 29.723
+  LoadShardBlock: 154.005 (3)
+  PetaboxLoader3.datanode: 283.976 (4)
+  load_resource: 286.862
+  PetaboxLoader3.resolve: 103.737
+-->
