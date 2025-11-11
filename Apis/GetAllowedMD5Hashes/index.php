74158f7ea4206007701b7a437b254529a3af16d9
TheGuyWhoIsIdiot <157973030+TheGuyWhoIsIdiot@users.noreply.github.com>
2025-11-11 17:31:32 -0300

    made client versions fully db sided.

--- Apis/GetAllowedMD5Hashes/index.php
+++ Apis/GetAllowedMD5Hashes/index.php
@@ -1,1 +1,11 @@
-{"data":["dd489d72565eea073e399f6d85d5d60d"]}
+<?php
+// written by meditext
+// this is responsible for returning allowed MD5 hashes, so the RCC can authorize the whitelisted client
+include_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
+header('Content-Type: application/json');
+$data = [];
+$all_clients = $conn->query("SELECT binary_checksum FROM clients");
+while ($row = $all_clients->fetch_assoc()) {
+    $data[] = $row['binary_checksum'];
+}
+exit(json_encode(["data" => $data]));
--- Apis/GetAllowedSecurityVersions/index.php
+++ Apis/GetAllowedSecurityVersions/index.php
@@ -1,1 +1,11 @@
-{"data":["0.203.0pcplayer","0.192.0pcplayer"]}
+<?php
+// written by meditext
+// this is responsible for returning allowed clients versions, so the RCC can authorize the whitelisted client
+include_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
+header('Content-Type: application/json');
+$data = [];
+$all_clients = $conn->query("SELECT release_version, release_type FROM clients");
+while ($row = $all_clients->fetch_assoc()) {
+    $data[] = $row['release_version'].$row['release_type']; // output: 0.123.0clienttype
+}
+exit(json_encode(["data" => $data]));
