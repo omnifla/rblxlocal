008e87aea774b2517d43157cb9affe5bca0a0097
TheGuyWhoIsIdiot <157973030+TheGuyWhoIsIdiot@users.noreply.github.com>
2025-11-23 16:17:09 -0300

    Reverted changes from 8f1dc5b

--- Main/Game/PlaceLauncher.ashx.php
+++ Main/Game/PlaceLauncher.ashx.php
@@ -27,12 +27,17 @@ $cookie = $_COOKIE['_ROBLOSECURITY'] ?? 0; // maybe i should look more on the co
 $status = 2; // temporary
 $request = $_GET['request'] ?? "JoiningServer";
 $isPlayTogetherGame = $_GET['isPlayTogetherGame'] ?? false;
+// Source - https://stackoverflow.com/a
+// Posted by Ben, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-11-23, License - CC BY-SA 3.0
+
+$realIP = file_get_contents("http://ipecho.net/plain"); // unethical for now.
 
 // output
 $output = [
     "jobId" => $jobId,
     "status" => $status,
-    "joinScriptUrl" => $joinscriptURL . "?placeId={$placeId}&jobId={$placeId}&MachineAddress=capital-tile.gl.at.ply.gg&serverPort=47683",
+    "joinScriptUrl" => $joinscriptURL . "?placeId={$placeId}&jobId={$placeId}&MachineAddress={$realIP}",
     "authenticationUrl" => $authenticationURL . "?suggest={$cookie}",
     "authenticationTicket" => (string)$cookie,
     "message" => "Joining Server..."
